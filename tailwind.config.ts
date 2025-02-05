6372a04ac7396ec583ad06c75c090e2816d88254
Henrique Moura <henrique.bmoura@yahoo.com.br>
2025-02-05 11:47:17 -0300

    Add dark mode support and extend responsive breakpoints in Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,30 @@ import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	darkMode: 'class',
 	theme: {
+		screens: {
+			"1xs": "375px",
+			// => @media (min-width: 375px) { ... }
+	  
+			xs: "450px",
+			// => @media (min-width: 450px) { ... }
+	  
+			sm: "575px",
+			// => @media (min-width: 576px) { ... }
+	  
+			md: "768px",
+			// => @media (min-width: 768px) { ... }
+	  
+			lg: "992px",
+			// => @media (min-width: 992px) { ... }
+	  
+			xl: "1200px",
+			// => @media (min-width: 1200px) { ... }
+	  
+			"2xl": "1400px",
+			// => @media (min-width: 1400px) { ... }
+		},
 		extend: {
 			colors: {
 				primary: {
