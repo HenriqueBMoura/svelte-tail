7b71d1ea0d436b37dfb5e451f2029e45d98bf7eb
Henrique Moura <henrique.bmoura@yahoo.com.br>
2025-02-07 21:54:00 -0300

    add dark mode class

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-    import BookForm from '$lib/components/BookForm.svelte';
-    import BookSearch from '$lib/components/BookSearch.svelte';
-    import BookList from '$lib/components/BookList.svelte';
-  </script>
-  
-  <div class="container mx-auto px-4 py-8">
-    <BookForm />
-    <BookSearch />
-    <!-- <BookList /> -->
-  </div>
+	import BookForm from '$lib/components/BookForm.svelte';
+	import BookSearch from '$lib/components/BookSearch.svelte';
+	import BookList from '$lib/components/BookList.svelte';
+</script>
+
+<div class="container bg-secondary-50 dark:bg-secondary-900 mx-auto px-4 py-8">
+	<BookForm />
+	<BookSearch />
+	<!-- <BookList /> -->
+</div>
