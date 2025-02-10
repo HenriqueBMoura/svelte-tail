cec1d3206a731751117470126df7c8feaa38cc99
Henrique Moura <henrique.bmoura@yahoo.com.br>
2025-02-10 17:54:01 -0300

    add ToastContainer component for managing toast notifications

---
+++ src/lib/components/ToastContainer.svelte
@@ -0,0 +1,15 @@
+<!-- <script>
+    import { toast } from '@zerodevx/svelte-toast';
+  </script>
+  
+  {#each $toast as { id, ...rest }}
+    <div {...rest}></div>
+  {/each} -->
+
+  <script>
+    import { toast } from '@zerodevx/svelte-toast';
+  </script>
+  
+  {#each $toast as { id, ...rest }}
+    <div {...rest}></div>
+  {/each}
