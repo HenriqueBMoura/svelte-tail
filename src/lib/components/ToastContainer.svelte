c231bc3711a2b54e4f77548c5881bfa5315de5c4
Henrique Moura <henrique.bmoura@yahoo.com.br>
2025-02-10 17:54:49 -0300

    refactor ToastContainer component to streamline toast rendering and improve code clarity

--- src/lib/components/ToastContainer.svelte
+++ src/lib/components/ToastContainer.svelte
@@ -1,15 +1,13 @@
-<!-- <script>
-    import { toast } from '@zerodevx/svelte-toast';
-  </script>
-  
-  {#each $toast as { id, ...rest }}
-    <div {...rest}></div>
-  {/each} -->
-
-  <script>
+<script>
     import { toast } from '@zerodevx/svelte-toast';
   </script>
   
   {#each $toast as { id, ...rest }}
     <div {...rest}></div>
   {/each}
+
+  <!-- <script>
+    import { ToastContainer } from '@zerodevx/svelte-toast';
+  </script>
+  
+  <ToastContainer /> -->
